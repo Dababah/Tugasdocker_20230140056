d28a3dd968b802568ca22b36137e0f859385730d
Your Name <your.email@example.com>
2026-04-13 11:47:14 +0700

    menambahkan model User dengan atribut nama, nim, dan jenis kelamin

---
+++ src/main/java/com/example/dockerfiledevelop/model/User.java
@@ -0,0 +1,15 @@
+package com.example.dockerfiledevelop.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
+}
